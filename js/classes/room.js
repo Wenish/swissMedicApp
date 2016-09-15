12d8d15a1e2fc2ad408bc1b53c02ed104f8a5e01
Wenish <jonas.voland@bluewin.ch>
2016-09-16 01:56:26 +0200

    extended class

--- js/classes/room.js
+++ js/classes/room.js
@@ -1,4 +1,8 @@
 var app = app || {};
-app.Room = function(title, options) {
-    this.title = title
+app.Room = function(options) {
+    this.id = options.id
+    this.title = options.title
+    this.created = options.created
+    this.serverRelativeUrl = options.serverRelativeUrl
+    this.lastItemModifiedDate = ko.observable(options.lastItemModifiedDate)
 };
